434daf2c405460a0c5fc6dbccfb354f0d04fecdf
olegka96 <oleg@bilbo.quantumbioinc.com>
2021-11-12 16:13:56 -0500

    Updated tutorials

--- Tutorials/phenix_divconTutorials.sh
+++ Tutorials/phenix_divconTutorials.sh
@@ -22,16 +22,16 @@ currentDate=`date`
 echo "BEGIN PHENIX/DivCon Tutorial Test at ${currentDate} using ${DIVCON_BIN}"
 
 WORKDIR=$PWD
-
-echo "Tutorial #1 (Running Phenix/DivCon without the qbphenix wrapper): 1IG3"
+PDBID=2WOR
+echo "Tutorial #1 (Running Phenix/DivCon without the qbphenix wrapper): $PDBID"
 rm -rf ${WORKDIR}/NakedPHENIX ; mkdir -p ${WORKDIR}/NakedPHENIX ; cd ${WORKDIR}/NakedPHENIX
 
-wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/1ig3.pdb
-wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/1ig3-sf.cif
+wget https://downloads.quantumbioinc.com/media/tutorials/XModeScore/2WOR.pdb
+wget https://downloads.quantumbioinc.com/media/tutorials/XModeScore/2WOR-sf.cif
 
-${PHENIX}/build/bin/phenix.ready_set 1ig3.pdb add_h_to_water=true
+${PHENIX}/build/bin/phenix.ready_set ${PDBID}.pdb add_h_to_water=true
 
-${PHENIX}/build/bin/phenix.refine 1ig3.updated.pdb 1ig3-sf.cif 1ig3.ligands.cif qblib=True qblib_method=pm6 qblib_region_selection="chain A and resname VIB and resid 502" qblib_region_radius=3.0 qblib_buffer_radius=3.0 qblib_np=4
+${PHENIX}/build/bin/phenix.refine ${PDBID}.updated.pdb ${PDBID}-sf.cif ${PDBID}.ligands.cif output.write_geo_file=False  output.write_def_file=False refinement.refine.strategy=individual_sites+individual_adp main.number_of_macro_cycles=2 qblib=True qblib_method=pm6 qblib_region_selection="chain A and resname 2AN and resid 1098" qblib_region_radius=3.0 qblib_buffer_radius=2.5 qblib_np=4 qblib_target_method=3 qblib_gradient_target=3
 
 echo "Tutorial #2a (using qbphenix execution script and MOE on single ligand): 3IX1"
 rm -rf ${WORKDIR}/qbphenix_moe ; mkdir -p ${WORKDIR}/qbphenix_moe ; cd ${WORKDIR}/qbphenix_moe
@@ -39,12 +39,12 @@ rm -rf ${WORKDIR}/qbphenix_moe ; mkdir -p ${WORKDIR}/qbphenix_moe ; cd ${WORKDIR
 wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3ix1.pdb
 wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3ix1-sf.cif
 
-$QBHOME/bin/qbphenix --dataFile 3ix1-sf.cif --pdbFile 3ix1.pdb --selection "chain A resname NFM resid 401"  --phenixOptions "main.number_of_macro_cycles=1" --protonation MOE --qmMethod pm6 --region-radius 3.0 --buffer-radius 2.5 --Nproc 4
+$QBHOME/bin/qbphenix --dataFile 3ix1-sf.cif --pdbFile 3ix1.pdb --selection "chain A resname NFM resid 401"  --phenixOptions "main.number_of_macro_cycles=2" --protonation MOE --qmMethod pm6 --region-radius 3.0 --buffer-radius 2.5 --Nproc 4
 
 echo "Tutorial #2b (using qbphenix execution script and phenix.ready_set on single ligand): 3IX1"
 rm -rf ${WORKDIR}/qbphenix_readyset ; mkdir -p ${WORKDIR}/qbphenix_readyset ; cd ${WORKDIR}/qbphenix_readyset
 
-$QBHOME/bin/qbphenix --pdbID 3ix1 --selection "chain A resname NFM resid 401" --phenixOptions "main.number_of_macro_cycles=1" --protonation ReadySet  --qmMethod pm6 --region-radius 3.0 --buffer-radius 2.5  --Nproc 4
+$QBHOME/bin/qbphenix --pdbID 3ix1 --selection "chain A resname NFM resid 401" --phenixOptions "main.number_of_macro_cycles=2" --protonation ReadySet  --qmMethod pm6 --region-radius 3.0 --buffer-radius 2.5  --Nproc 4
 
 echo "Tutorial #3 (using qbphenix execution script and MOE on all ligands): 3IX1"
 rm -rf ${WORKDIR}/qbphenix_moe_all ; mkdir -p ${WORKDIR}/qbphenix_moe_all ; cd ${WORKDIR}/qbphenix_moe_all
@@ -52,7 +52,7 @@ rm -rf ${WORKDIR}/qbphenix_moe_all ; mkdir -p ${WORKDIR}/qbphenix_moe_all ; cd $
 wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3ix1.pdb
 wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3ix1-sf.cif
 
-$QBHOME/bin/qbphenix --dataFile 3ix1-sf.cif --pdbFile 3IX1.pdb --selection "resname NFM" --phenixOptions "main.number_of_macro_cycles=1" --protonation MOE  --qmMethod pm6 --region-radius 3.0 --buffer-radius 2.5  --Nproc 4  
+$QBHOME/bin/qbphenix --dataFile 3ix1-sf.cif --pdbFile 3ix1.pdb --selection "resname NFM" --phenixOptions "main.number_of_macro_cycles=1" --protonation MOE  --qmMethod pm6 --region-radius 3.0 --buffer-radius 2.5  --Nproc 4  
 
 echo "Tutorial #4 (using qbphenix execution script and MOE): 1LRI "
 rm -rf ${WORKDIR}/qbphenix_moe_1lri ; mkdir -p ${WORKDIR}/qbphenix_moe_1lri ; cd ${WORKDIR}/qbphenix_moe_1lri
@@ -60,17 +60,17 @@ rm -rf ${WORKDIR}/qbphenix_moe_1lri ; mkdir -p ${WORKDIR}/qbphenix_moe_1lri ; cd
 wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/1lri.pdb
 wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/1lri-sf.cif
 
-$QBHOME/bin/qbphenix --dataFile 1lri-sf.cif --pdbFile 1LRI.pdb --selection "chain A resname CLR resid 99"  --phenixOptions "main.number_of_macro_cycles=1" --protonation MOE --qmMethod pm6 --region-radius 3.0 --buffer-radius 2.5 --Nproc 4 
+$QBHOME/bin/qbphenix --dataFile 1lri-sf.cif --pdbFile 1lri.pdb --selection "chain A resname CLR resid 99"  --phenixOptions "main.number_of_macro_cycles=2" --protonation MOE --qmMethod pm6 --mmMethod amberff14sb --region-radius 3.0 --buffer-radius 2.5 --Nproc 4 
 
 echo "Tutorial #5: ONIOM (QM/MM) Based X-ray Refinement and Phenix Clash Score Analysis"
 rm -rf ${WORKDIR}/Protonation ; mkdir -p ${WORKDIR}/Protonation ; cd ${WORKDIR}/Protonation
 
-$QBHOME/bin/qbphenix --pdbID 1NAV --mmMethod amberff14 --qmMethod pm6 --selection "resname IH5" --np 4 --region-radius 3.0 --buffer-radius 0.0 --protonation MOE >& OUT.screen
+$QBHOME/bin/qbphenix --pdbID 1NAV --mmMethod amberff14sb --qmMethod pm6 --selection "resname IH5" --np 4 --region-radius 3.0 --buffer-radius 0.0 --protonation MOE >& OUT.screen
 
 echo "Tutorial #6 (using qbphenix execution script and MOE on covalently bound ligand): 3NCK"
 rm -rf ${WORKDIR}/Protonation ; mkdir -p ${WORKDIR}/Protonation ; cd ${WORKDIR}/Protonation
 
-$QBHOME/bin/qbphenix --pdbID 3NCK --selection "resname NFF" --phenixOptions "main.number_of_macro_cycles=1" --protonation MOE  --qmMethod pm6 --region-radius 3.0 --buffer-radius 2.5  --Nproc 4  --scriptName run
+$QBHOME/bin/qbphenix --pdbID 3NCK --selection "resname NFF" --phenixOptions "main.number_of_macro_cycles=2" --protonation MOE  --qmMethod pm6 --mmMethod amberff14sb --region-radius 3.0 --buffer-radius 2.5  --Nproc 4  --scriptName run
 
 grep -v "H1  NFF A" 3NCK.pdb > tmp.$$ ; mv tmp.$$ 3NCK.pdb
 grep -v "H3  NFF A" 3NCK.pdb > tmp.$$ ; mv tmp.$$ 3NCK.pdb
--- Tutorials/xmodescoreTutorials.sh
+++ Tutorials/xmodescoreTutorials.sh
@@ -26,40 +26,42 @@ WORKDIR=$PWD
 echo "Tutorial #1: XModeScore executed on PDBid:1OPK"
 rm -rf ${WORKDIR}/xmodeScore_1OPK ; mkdir -p ${WORKDIR}/xmodeScore_1OPK ; cd ${WORKDIR}/xmodeScore_1OPK
 
-$QBHOME/bin/qbphenix --pdbID 1OPK --XModeScore --protonation MOE --selection "chain A resname 18K resid 299" --np 20 --dir test1
+$QBHOME/bin/qbphenix --pdbID 4YJR --XModeScore --protomers "-1..1" --mmMethod amberff14sb --qmMethod pm6 --protonation MOE --selection "chain A resname 4DJ resid 701" --np 20 --dir test1
 
-echo "Tutorial 2a: XModeScore on AZM PDBid:3HS4"
+echo "Tutorial 2a: XModeScore on user-provided AZM 3HS4.pdb file"
 rm -rf ${WORKDIR}/xmodeScore_3HS4 ; mkdir -p ${WORKDIR}/xmodeScore_3HS4 ; cd ${WORKDIR}/xmodeScore_3HS4
-
-$QBHOME/bin/qbphenix --pdbID 3HS4 --XModeScore --protonation MOE --nproc 20 --dir xmodeData --selection "chain A resname AZM resid 701" --protPH 9.0 --generateTautomers divcon --protomers "-1..0"
+wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3HS4-tautomers/3HS4+H.pdb
+wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3HS4-tautomers/3HS4.mtz
+$QBHOME/bin/qbphenix --pdbFile 3HS4+H.pdb --dataFile  3HS4.mtz --XModeScore --protomers "-1..0" --mmMethod amberff14sb --qmMethod pm6 --protonation skip --nproc 20 --dir xmodeData --selection "chain A resname AZM resid 701" --protPH 9.0 --generateTautomers divcon 
     
-echo "Tutorial 2b: XModeScore on user-provided AZM 3HS4.pdb file"
-rm -rf ${WORKDIR}/xmodeScore_user3HS4 ; mkdir -p ${WORKDIR}/xmodeScore_user3HS4 ; cd ${WORKDIR}/xmodeScore_user3HS4
-
-wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3HS4.pdb
-wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3HS4.sf
-
-$QBHOME/bin/qbphenix --pdbFile 3HS4.pdb --dataFile  3HS4.mtz --cifFile  3HS4.cif --XModeScore --protonation skip --nproc 20 --dir xmodeData --selection "chain A resname AZM resid 701" --protPH 9.0 --generateTautomers divcon --protomers "-1..0"
-
-echo "Tutorial 2c: XModeScore on pre-generated files"
+echo "Tutorial 2b: XModeScore on pre-generated files"
 rm -rf ${WORKDIR}/xmodeScore_pregen3HS4 ; mkdir -p ${WORKDIR}/xmodeScore_pregen3HS4 ; cd ${WORKDIR}/xmodeScore_pregen3HS4
+wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3HS4-tautomers/3HS4+H.pdb
+wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3HS4-tautomers/3HS4.mtz
+mkdir 3HS4-tautomers; cd 3HS4-tautomers
+wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3HS4-tautomers/0_0_0_0_-1.pdb
+wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3HS4-tautomers/1_1_0_0_-1.pdb
+wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3HS4-tautomers/3_1_0_2_-1.pdb
+wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3HS4-tautomers/4_0_0_0_0.pdb
+wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/3HS4-tautomers/6_1_0_1_0.pdb
+cd ../
+$QBHOME/bin/qbphenix --pdbFile 3HS4+H.pdb --dataFile  3HS4.mtz --XModeScore 3HS4-tautomers --selection "chain A resname AZM resid 701" --mmMethod amberff14sb --qmMethod pm6 --protonation skip --protonateTautomers skip --nproc 20 --dir xmodeData --v 1 
 
-#INSERT EXAMPLE HERE
+echo "Tutorial #3: XModeScore on Buster structures"
+rm -rf ${WORKDIR}/xmodeScore_external_2BSM ; mkdir -p ${WORKDIR}/xmodeScore_external_2BSM ; cd ${WORKDIR}/xmodeScore_external_2BSM
+source /share/apps/GlobalPhasing/linux-x86_64/BUSTER_snapshot_20180515/setup.sh
+wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/2BSM+H.pdb
+wget http://downloads.quantumbioinc.com/media/tutorials/XModeScore/2BSM.mtz
+qbbuster --pdbFile 2BSM+H.pdb --sfFile 2BSM.mtz --XModeScore --protomers "-1..1" --protonation skip --makeCIF grade --mmMethod amberff14sb --qmMethod pm6 --qmWeight 5.0 --ncycles 1 --resname BSM --chain A --resid 1224 --np 20 --buffer-radius 0.0 --region-radius 3.0
 
-echo "Tutorial #3: XModeScore on Refmac/Buster structures"
-rm -rf ${WORKDIR}/xmodeScore_external1TOW ; mkdir -p ${WORKDIR}/xmodeScore_external1TOW ; cd ${WORKDIR}/xmodeScore_external1TOW
+exit
+echo "Tutorial #4: Automated MOE-based Docking – using qbphenix"
+dir4=xmodeScore_autoDock
+rm -rf ${WORKDIR}/$dir4 ; mkdir -p ${WORKDIR}/$dir4 ; cd ${WORKDIR}/$dir4
 
-#INSERT EXAMPLE HERE
-
-echo "Tutorial #4: ONIOM (QM/MM) XModeScore on PDBid:3HS4"
-rm -rf ${WORKDIR}/xmodeScore_ONIOM_3HS4 ; mkdir -p ${WORKDIR}/xmodeScore_ONIOM_3HS4 ; cd ${WORKDIR}/xmodeScore_ONIOM_3HS4
-
-$QBHOME/bin/qbphenix --pdbid 3HS4 --XmodeScore --protonation moe --nproc 20 --dir xmodeData --selection "chain A resname AZM resid 701" --protPH 9.0 --generateTautomers divcon --protomers "-1,0" --mmMethod amberff14 --qmMethod pm6 --buffer-radius 0.0 --region-radius 3.0
+$QBHOME/bin/qbphenix --pdbFile 5C3K-H_refine_001.pdb --dataFile 5C3K.mtz --densityFile 5C3K-H_refine_001.mtz --ligandFile 4XF_A_402.pdb --dock dockFolderResults --protonation skip --Xmodescore --protonateTautomers MOE --mmMethod amberff14 --qmMethod pm6 --region-radius 3.0 --nproc 22
     
-echo "Automated MOE-based Docking – using qbphenix"
-rm -rf ${WORKDIR}/xmodeScore_MOEDOCK_3HS4 ; mkdir -p ${WORKDIR}/Protonation ; cd ${WORKDIR}/Protonation
 
-# INSERT EXAMPLE HERE
 
 
 currentDate=`date`
