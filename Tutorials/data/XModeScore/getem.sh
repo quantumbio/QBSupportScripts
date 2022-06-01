a3967aa14807f7cb11ecbd097d95f1cbc8fb6a49
LanceWesterhoff <lance@quantumbioinc.com>
2022-06-01 14:15:16 -0400

    addition of input data for the tutorials - use wget raw.githubusercontent.com for these files within the sh scripts

---
+++ Tutorials/data/XModeScore/getem.sh
@@ -0,0 +1,2 @@
+wget http://downloads.quantumbioinc.com/media/tutorials/RealSpaceStats/0_0_0_0_-1_refine_001.mtz
+wget http://downloads.quantumbioinc.com/media/tutorials/RealSpaceStats/0_0_0_0_-1_refine_001.pdb
