ecefb7df229a087bc1d2ce7c05df8a42d46312b7
oleg <oleg@bilbo.quantumbioinc.com>
2022-06-08 10:48:33 -0400

    Xray-divcon tutorial

--- Tutorials/xray_divconTutorials.sh
+++ Tutorials/xray_divconTutorials.sh
@@ -23,3 +23,20 @@ if [ -z "${QBHOME}" ]; then
     exit
 fi
 
+
+DIVCON_BIN=${QBHOME}/bin/qmechanic
+
+if [ ! -x "${DIVCON_BIN}" ]; then
+    echo "ERROR: ${DIVCON_BIN} is not an executable! Set this path to the qmechanic executable"
+    exit
+fi
+
+currentDate=`date`
+echo "BEGIN DivCon Xrya Tutorial Test at ${currentDate} using ${DIVCON_BIN}"
+
+WORKDIR=$PWD
+
+echo "Tutorial #1: XModeScore executed on PDBid:1OPK"
+rm -rf ${WORKDIR}/xmodeScore_1OPK ; mkdir -p ${WORKDIR}/xmodeScore_1OPK ; cd ${WORKDIR}/xmodeScore_1OPK
+
+$QBHOME/bin/qbphenix --pdbID 4YJR --XModeScore --protomers "-1..1" --mmMethod amberff14sb --qmMethod pm6 --protonation MOE --selection "chain A resname 4DJ resid 701" --np 20 --dir test1
