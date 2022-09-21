c8f1975cf711bfc77b13fae0e3d5c174f6064f10
oleg <oleg@bilbo.quantumbioinc.com>
2022-09-21 11:27:29 -0400

    Cryo_EM tutorial

--- Tutorials/xray_divconTutorials.sh
+++ Tutorials/xray_divconTutorials.sh
@@ -60,5 +60,14 @@ wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutori
 
 $QBHOME/bin/qbphenix --pdbFile 4b72+H.pdb --sfFile 4b72.mtz --XmodeScore --protonation Skip --protomers "-1..1" --engine divcon --protonateTautomers divcon --qmMethod pm6 --mmMethod amberff14sb --nSmallCycles 50 --region-radius 3.0 --buffer-radius 0.0 --selection "resname 2FB and resid 1503 and chain A" --dir testXmode --Nproc 16
 
+echo "Tutorial #4: Cryo_EM Refinement on protonated PDBid:7jsy"
+tutorFolder=cryoEM_7jsy
+rm -rf ${WORKDIR}/$tutorFolder ; mkdir -p ${WORKDIR}/$tutorFolder ; cd ${WORKDIR}/$tutorFolder
+wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/7jsy+H.pdb
+wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/emd_22463.map
+
+$QBHOME/bin/qmechanic 7jsy+H.pdb emd_22463.map --opt all 35 0.01 --qm-region /A/I3C/501// 0.0 0.0 -h pm6 amberff14sb -O -p 7jsy+H_refined.pdb 7jsy+H_refined.mtz --np 4 -v 2 --nb-cutoff 25
+
+
 currentDate=`date`
 echo "END Tutorial Test at ${currentDate} using ${DIVCON_BIN}"
