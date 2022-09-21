6a64eee7625c1c24675945d5432b095fdc17b465
oleg <oleg@bilbo.quantumbioinc.com>
2022-09-21 17:32:57 -0400

    updated CryoEM tutorial

--- Tutorials/xray_divconTutorials.sh
+++ Tutorials/xray_divconTutorials.sh
@@ -57,6 +57,9 @@ tutorFolder=xmodeScore_4b72
 rm -rf ${WORKDIR}/$tutorFolder ; mkdir -p ${WORKDIR}/$tutorFolder ; cd ${WORKDIR}/$tutorFolder
 wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/XModeScore/4b72+H.pdb
 wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/XModeScore/4b72.mtz
+export ELECTRON=1
+export SKIP_BFAC=1
+export DEV_XRAY_WEIGHT=0.5
 
 $QBHOME/bin/qbphenix --pdbFile 4b72+H.pdb --sfFile 4b72.mtz --XmodeScore --protonation Skip --protomers "-1..1" --engine divcon --protonateTautomers divcon --qmMethod pm6 --mmMethod amberff14sb --nSmallCycles 50 --region-radius 3.0 --buffer-radius 0.0 --selection "resname 2FB and resid 1503 and chain A" --dir testXmode --Nproc 16
 
@@ -65,6 +68,9 @@ tutorFolder=cryoEM_7jsy
 rm -rf ${WORKDIR}/$tutorFolder ; mkdir -p ${WORKDIR}/$tutorFolder ; cd ${WORKDIR}/$tutorFolder
 wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/7jsy+H.pdb
 wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/emd_22463.map
+export ELECTRON=1
+export SKIP_BFAC=1
+export DEV_XRAY_WEIGHT=0.5
 
 $QBHOME/bin/qmechanic 7jsy+H.pdb emd_22463.map --opt all 35 0.01 --qm-region /A/I3C/501// 0.0 0.0 -h pm6 amberff14sb -O -p 7jsy+H_refined.pdb 7jsy+H_refined.mtz --np 4 -v 2 --nb-cutoff 25
 
