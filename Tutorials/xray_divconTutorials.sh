909bf788c281c2872544c5301b830a31e4d05fea
oleg <oleg@bilbo.quantumbioinc.com>
2023-09-06 15:49:01 -0400

    cryo em tutorial

--- Tutorials/xray_divconTutorials.sh
+++ Tutorials/xray_divconTutorials.sh
@@ -64,15 +64,32 @@ tutorFolder=cryoEM_7jsy
 rm -rf ${WORKDIR}/$tutorFolder ; mkdir -p ${WORKDIR}/$tutorFolder ; cd ${WORKDIR}/$tutorFolder
 wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/7jsy+H.pdb
 wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/emd_22463.map
-export ELECTRON=1
-export SKIP_BFAC=1
-export SETRESOL=1.8
-#export DEV_XRAY_WEIGHT=0.5
 
-$QBHOME/bin/qmechanic 7jsy+H.pdb emd_22463.map --opt all 35 0.01 --qm-region /A/I3C/501// 0.0 0.0 -h pm6 amberff14sb -O -p 7jsy+H_refined.pdb 7jsy+H_refined.mtz --np 4 -v 2 --nb-cutoff 25
+$QBHOME/bin/qmechanic 7jsy+H.pdb emd_22463.map --resolution 1.8 --experiment cryoEM --opt all 35 0.01 --qm-region /A/I3C/501// 0.0 0.0 -h pm6 amberff14sb -O -p 7jsy+H_refined.pdb 7jsy+H_refined.mtz --np 4 -v 2 --nb-cutoff 25
+
+#
+echo "Tutorial #5: Cryo_EM ONIOM Refinement with qbdivcon: PDBid:7efc"
+tutorFolder=cryoEM_qbdivcon_7efc
+rm -rf ${WORKDIR}/$tutorFolder ; mkdir -p ${WORKDIR}/$tutorFolder ; cd ${WORKDIR}/$tutorFolder
+
+wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/7efc+H.pdb
+wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/emd_31083.map
+
+$QBHOME/bin/qbdivcon --pdbfile 7efc+H.pdb --sfFile emd_31083.map --protonation skip  --engine divcon --qmMethod pm6 --mmMethod amberff14sb --resname BTN --chain A --resid 5100 --np 4 --region-radius 3.0 --nSmallCycles 40 
+#
+
+echo "Tutorial #6: Cryo_EM XModeScore with qbdivcon: PDBid:7jsy"
+tutorFolder=cryoEM_xmodescore_7jsy
+rm -rf ${WORKDIR}/$tutorFolder ; mkdir -p ${WORKDIR}/$tutorFolder ; cd ${WORKDIR}/$tutorFolder
+
+wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/7jsy+H.pdb
+wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/emd_22463.map
+
+$QBHOME/bin/qbdivcon --pdbfile 7jsy+H.pdb --sfFile emd_22463.map --XmodeScore --protomers "0" --exploreFlip --protonation skip  --engine divcon --qmMethod pm6 --mmMethod amberff14sb --resname I3C --chain A --resid 501 --np 16 --region-radius 3.0 --nSmallCycles 40 --dir testXmode
+#
 
 
-echo "Tutorial #5: XModeScore with Dock executed on protonated PDBid:1bzc"
+echo "Tutorial #7: XModeScore with Dock executed on protonated PDBid:1bzc"
 tutorFolder=xmodeScore_1bzc
 rm -rf ${WORKDIR}/$tutorFolder ; mkdir -p ${WORKDIR}/$tutorFolder ; cd ${WORKDIR}/$tutorFolder
 wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/XModeScore/1bzc+H.pdb
