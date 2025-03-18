f15c76034ca485d0447e9993853cfca4543e3c37
olegka96 <oleg@bilbo.quantumbioinc.com>
2025-03-18 14:31:49 -0400

    cryoEM tutorial with qmechanic

--- Tutorials/cryoem_divconTutorials.sh
+++ Tutorials/cryoem_divconTutorials.sh
@@ -45,7 +45,7 @@ wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutori
 $QBHOME/bin/qmechanic 7jsy+H.pdb emd_22463.map --resolution 1.8 --experiment cryoEM --opt all 35 0.01 --qm-region /A/I3C/501// 0.0 0.0 -h pm6 amberff14sb -O -p 7jsy+H_refined.pdb 7jsy+H_refined.mtz --np 4 -v 2 --nb-cutoff 25
 
 #
-echo "Tutorial #2: Cryo_EM ONIOM Refinement with qbdivcon: PDBid:7efc"
+echo "Tutorial #2: Cryo_EM ONIOM Refinement with qbDivCon: PDBid:7efc"
 tutorFolder=cryoEM_qbdivcon_7efc
 rm -rf ${WORKDIR}/$tutorFolder ; mkdir -p ${WORKDIR}/$tutorFolder ; cd ${WORKDIR}/$tutorFolder
 
@@ -55,7 +55,7 @@ wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutori
 $QBHOME/bin/qbdivcon --pdbfile 7efc+H.pdb --sfFile emd_31083.map --experiment cryoEM --resolution 1.7 --protonation skip  --engine divcon --qmMethod pm6 --mmMethod amberff14sb --resname BTN --chain A --resid 5100 --np 4 --region-radius 3.0 --nSmallCycles 40 
 #
 
-echo "Tutorial #3: Cryo_EM XModeScore with qbdivcon: PDBid:7jsy"
+echo "Tutorial #3: Cryo_EM XModeScore with qbDivCon: PDBid:7jsy"
 tutorFolder=cryoEM_xmodescore_7jsy
 rm -rf ${WORKDIR}/$tutorFolder ; mkdir -p ${WORKDIR}/$tutorFolder ; cd ${WORKDIR}/$tutorFolder
 
@@ -64,7 +64,14 @@ wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutori
 
 $QBHOME/bin/qbdivcon --pdbfile 7jsy+H.pdb --sfFile emd_22463.map --experiment cryoEM --resolution 1.8 --XmodeScore --protomers "-1..1" --exploreFlip --protonation skip  --engine divcon --qmMethod pm6 --mmMethod amberff14sb --resname I3C --chain A --resid 501 --np 16 --region-radius 3.0 --nSmallCycles 40 --dir testXmode
 #
+echo "Tutorial #4: Cryo_EM XModeScore with DivCon qmechanic: PDBid:7jsy"
+tutorFolder=cryoEM_qmechanic_xmodescore_7jsy
+rm -rf ${WORKDIR}/$tutorFolder ; mkdir -p ${WORKDIR}/$tutorFolder ; cd ${WORKDIR}/$tutorFolder
+wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/7jsy+H.pdb
+wget https://raw.githubusercontent.com/quantumbio/QBSupportScripts/master/Tutorials/data/CryoEM/emd_22463.map
 
+$QBHOME/bin/qmechanic 7jsy+H.pdb emd_22463.map --resolution 1.8 --experiment cryoEM --ligand /A/I3C/501// --protomer [-1..1] /A/I3C/501// --flip on --chirality on --np 8 -h amberff14sb --dock LIGAND --xmodescore opt all target 40 0.1 -v2
+#
 currentDate=`date`
 echo "END Tutorial Test at ${currentDate} using ${DIVCON_BIN}"
 
